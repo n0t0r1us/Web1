7316bfc30ade5b0f3f55813f03b77d9ed460aa10
n0t0r1us <conganolaca2869@gmail.com>
2021-10-08 12:53:39 +0700

    Exercise

---
+++ BaiTap1_61131562/BaiTap1_61131562/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaiTap1_61131562.MvcApplication" Language="C#" %>
---
+++ BaiTap1_61131562/BaiTap1_61131562/Models/CalModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap1_61131562.Models
+{
+    public class CalModels
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public string pt { get; set; }
+    }
+}
