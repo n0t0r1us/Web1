7e63f9cf402ea52a015189891bbe7b8b07e63d08
n0t0r1us <conganolaca2869@gmail.com>
2021-10-20 14:49:49 +0700

    Exercise

--- BaiTap1_61131562/BaiTap1_61131562/Controllers/PhepToan_61131562Controller.cs
+++ BaiTap1_61131562/BaiTap1_61131562/Controllers/PhepToan_61131562Controller.cs
@@ -49,5 +49,43 @@ namespace BaiTap1_61131562.Controllers
             }
             return View();
         }
+        public ActionResult UseArguments()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UseArguments(double a, double b, string pt = "+")
+        {
+            switch (pt)
+            {
+                case "+": ViewBag.KQ = a + b; break;
+                case "-": ViewBag.KQ = a - b; break;
+                case "*": ViewBag.KQ = a * b; break;
+                case "/":
+                    if (b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = a / b; break;
+            }
+            return View();
+        }        public ActionResult UseFormCollection()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UseFormCollection(FormCollection f)
+        {
+            double a = double.Parse(f["a"]);//Chuyển đổi chuỗi sang số thực
+            double b = double.Parse(f["b"]);
+            string pt = f["pt"].ToString();
+            switch (pt)
+            {
+                case "+": ViewBag.KQ = a + b; break;
+                case "-": ViewBag.KQ = a - b; break;
+                case "*": ViewBag.KQ = a * b; break;
+                case "/":
+                    if (b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = a / b; break;
+            }
+            return View();
+        }
     }
 }
